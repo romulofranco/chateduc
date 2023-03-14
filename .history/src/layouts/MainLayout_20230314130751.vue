bcf8fe06b5b3c40587794f2aa71c2d3356c5d0cb
Romulo Franco <romulojosefranco@gmail.com>
2023-03-14 13:54:12 -0300

    First commit with Login Supabase

---
+++ .history/src/layouts/MainLayout_20230314130751.vue
@@ -0,0 +1,113 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <q-btn-dropdown flat color="white" icon="person">
+          <q-list>
+            <q-item clickable v-close-popup @click="handlerLogout">
+              <q-item-section>
+                <q-item-label>Logout</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+import { useQuasar } from "quasar";
+
+import EssentialLink from "components/EssentialLink.vue";
+import userAuthUser from "../composables/UserAuthUser";
+import useNotify from "src/composables/UseNotify";
+import useDialog from "src/composables/UseDialog";
+
+const linksList = [
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const router = useRouter();
+    const { logout } = userAuthUser();
+    const { notifyError, notifySuccess } = useNotify();
+    const { dialogShow } = useDialog();
+
+    const handlerLogout = async () => {
+      dialogShow({
+        tittle: "Sair",
+        message: "Deseja realmente sair da aplicação?",
+      })
+        .onOk(async () => {
+          try {
+            await logout();
+            notifySuccess("Bye bye! 😁");
+            router.replace({
+              name: "login",
+            });
+          } catch (error) {
+            notifyError(error.message);
+          }
+          /** o replace elimina o histórico de rotas, diferente
+           * do push, que adicionar na pilha de histórico
+           */
+        })
+        .onCancel(async () => {
+          notifySuccess("Ops, ia mas voltou! 😁");
+        });
+    };
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+      handlerLogout,
+    };
+  },
+});
+</script>
---
+++ src/boot/supabase.js
@@ -0,0 +1,20 @@
+import { createClient } from '@supabase/supabase-js';
+import useAuthUser from "src/composables/UserAuthUser";
+
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabaseUrl = 'https://okqyezbhisndtvyuzjey.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rcXllemJoaXNuZHR2eXV6amV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg3MjIyMzksImV4cCI6MTk5NDI5ODIzOX0.yT5bDvuB_m2KybwJRCsHXS32KZwLJ4gh40AcySNfkDw'
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+supabase.auth.onAuthStateChange((event, session) => {
+  const { user } = useAuthUser();
+  user.value = session?.user || null; // se user.value <> null, atualize.
+});
+
+//console.log('Init Supabase: ', supabase);
+
+export default function useSupabase () {
+  return { supabase };
+}
